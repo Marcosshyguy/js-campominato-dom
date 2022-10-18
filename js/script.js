52c670dee7682a35cc60c130cc448e7bf1d383ae
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-10-18 13:04:12 +0200

    create a bomb generator

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+// Generare una griglia di gioco quadrata in cui ogni cella contiene un numero compreso tra 1 e 100.
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// I numeri nella lista delle bombe non possono essere duplicati.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+/** -creo una griglia a con varie scatole a seconda del livello
+    -creo un randomizzatoreche generi 16 numeri che diverranno le bombe evitando di duplicare i numeri delle bombe 
+    quindi con while dovrò produrre 16 numeri riconoscendo se sono gia inclusi nella generazione
+    -attribuire alla lista generata questi 16 elementi e
+    quando questi elementi verranno cliccati la cella diverrà rossa e il gioco terminerà
+    se non vengono cliccati le celle possono continuare a essere premute 
+*/ 
+
+
 
 // grid
 const grid = document.querySelector(".row");
@@ -84,9 +100,25 @@ function boxGenerator(numberOfBoxes, levelBoxSize) {
     }
 }
 
-// create a random number generator with function that produce number between two ranges min max and that not prodeuce duplicated numbers
-// let randomNumber = 0
-// function getRandomNumber(min, max) {
-    //      randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
-    //     return randomNumber;
-    //   }
+
+
+
+// create a random number generator with function that produce number between two ranges min max and that doesn't produce duplicated numbers
+
+/**
+ * bomb number generator from 1 to how many bombs we want
+ * @param {number} bombNumber
+ * @returns {object} bombs  
+ */
+function getRandomNumber(bombNumber) {
+    const bombs = [];
+    while (bombs.length < bombNumber) {
+    const randomNumber = Math.floor(Math.random() * bombNumber + 1);
+    if (!bombs.includes(randomNumber)) {
+      bombs.push(randomNumber);
+    }
+  }
+    return bombs;
+}
+
+
